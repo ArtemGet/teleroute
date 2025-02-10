7b2426f378dd3b5f59cdb6836750f95ca75f4c2d
Artem Getmanskii <re910@yandex.ru>
2025-02-10 21:10:53 +0300

    shorten out the path from src/main/java/com/github/artemget/teleroute to src/io/github/artemget/teleroute

---
+++ src/io/github/artemget/teleroute/command/CmdBatch.java
@@ -0,0 +1,108 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2024-2025. Artem Getmanskii
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package io.github.artemget.teleroute.command;
+
+import io.github.artemget.teleroute.send.Send;
+import io.github.artemget.teleroute.send.SendBatch;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Execute many commands as one.
+ *
+ * @param <U> Update
+ * @param <C> Client
+ * @since 0.1.0
+ */
+public final class CmdBatch<U, C> implements Cmd<U, C> {
+    /**
+     * Logger.
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(CmdBatch.class);
+
+    /**
+     * Commands.
+     */
+    private final Collection<Cmd<U, C>> commands;
+
+    /**
+     * Ctor.
+     *
+     * @param commands Commands to execute
+     */
+    @SafeVarargs
+    public CmdBatch(final Cmd<U, C>... commands) {
+        this(Arrays.asList(commands));
+    }
+
+    /**
+     * Main ctor.
+     *
+     * @param commands Commands to execute
+     */
+    public CmdBatch(final Collection<Cmd<U, C>> commands) {
+        this.commands = Collections.unmodifiableCollection(commands);
+    }
+
+    @Override
+    public Optional<Send<C>> execute(final U update) {
+        final List<Send<C>> sends = this.executeCmds(update);
+        final Optional<Send<C>> resp;
+        if (sends.isEmpty()) {
+            resp = Optional.empty();
+        } else {
+            resp = Optional.of(new SendBatch<>(sends));
+        }
+        return resp;
+    }
+
+    private List<Send<C>> executeCmds(final U update) {
+        return this.commands.stream()
+            .map(
+                cmd -> {
+                    Optional<Send<C>> resp;
+                    try {
+                        resp = cmd.execute(update);
+                    } catch (final CmdException exception) {
+                        CmdBatch.LOG.warn(
+                            "Error execute cmd: {}",
+                            exception.getMessage(),
+                            exception
+                        );
+                        resp = Optional.empty();
+                    }
+                    return resp;
+                })
+            .filter(Optional::isPresent)
+            .map(Optional::get)
+            .collect(Collectors.toList());
+    }
+}
---
+++ test/io/github/artemget/teleroute/match/MatchAnyTest.java
@@ -0,0 +1,78 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2024-2025. Artem Getmanskii
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package io.github.artemget.teleroute.match;
+
+import io.github.artemget.teleroute.update.FkWrap;
+import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test case {@link MatchAny}.
+ *
+ * @since 0.1.0
+ */
+final class MatchAnyTest {
+
+    @Test
+    void matchesWhenNoConditionSpecified() {
+        MatcherAssert.assertThat(
+            "Didnt match without condition",
+            new MatchAny<String>().test(new FkWrap())
+        );
+    }
+
+    @Test
+    void matchesWhenAllMatch() {
+        MatcherAssert.assertThat(
+            "Didnt match when all match",
+            new MatchAny<>(
+                new FkMatch(),
+                new FkMatch()
+            ).test(new FkWrap())
+        );
+    }
+
+    @Test
+    void matchesWhenAnyMatch() {
+        MatcherAssert.assertThat(
+            "Didnt match when any match",
+            new MatchAny<>(
+                new FkMatch(false),
+                new FkMatch()
+            ).test(new FkWrap())
+        );
+    }
+
+    @Test
+    void matchesNotWhenNoneMatch() {
+        MatcherAssert.assertThat(
+            "Matched when all didnt match",
+            !new MatchAny<>(
+                new FkMatch(false),
+                new FkMatch(false)
+            ).test(new FkWrap())
+        );
+    }
+}
